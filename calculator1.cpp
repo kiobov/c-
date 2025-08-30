9db62fbfcea3e1d66204e341ff50b6767f7eed18
kiobov <nyakio19roseary@gmail.com>
2025-08-30 19:14:32 +0300

    added prompt an cleaned interface

--- calculator1.cpp
+++ calculator1.cpp
@@ -12,45 +12,42 @@ inline void keep_window_open()
 void show_calculator(){
 	cout<< "-------------------------------------------\n";
 	cout<< "---                                     ---\n";
-	cout<< "-               SCIENTIFIC                -\n";
+	cout<< "-               BASIC                     -\n";
 	cout<< "-               CALCULATOR                -\n";
 	cout<< "---                                     ---\n";
 	cout<< "---------------------------------------"<<"\n";
 	cout<< "-     +    |        -    |       *    -"<<"\n";
 	cout<< "-    Add   |      Minus  |    Multiply-"<<"\n";
 	cout<< "---------------------------------------"<<"\n";
-	cout<< "-     /    |        S    |       C    -"<<"\n";
-	cout<< "-   Divide |    Square   |      Cube  -"<<"\n";
-	cout<< "---------------------------------------"<<"\n";
-	cout<< "-    r     |        x    |       q    -"<<"\n";
-	cout<< "-  sq root |       clear |     quit   -"<<"\n";
+	cout<< "-    /     |        x    |       q    -"<<"\n";
+	cout<< "-  Divide  |       clear |     quit   -"<<"\n";
 	cout<< "---------------------------------------"<<"\n";
 }
 
-void display_output(double answer ){
+void display_output(double lval ){
 	cout<< "---------------------------------------"<<"\n";
 	cout<< "---                                 ---"<<"\n";
 	cout<< "-               Answer     |          -"<<"\n";
-	cout<< "-               Output:    |"<<answer<<"   \n";
+	cout<< "-               Output:    |"<<lval<<"     \n";
 	cout<< "---                                 ---"<<"\n";
 	cout<< "---------------------------------------"<<"\n";
 	cout<< "-     +    |        -    |       *    -"<<"\n";
 	cout<< "-    Add   |      Minus  |    Multiply-"<<"\n";
 	cout<< "---------------------------------------"<<"\n";
-	cout<< "-     /    |        S    |       C    -"<<"\n";
-	cout<< "-   Divide |    Square   |      Cube  -"<<"\n";
-	cout<< "---------------------------------------"<<"\n";
-	cout<< "-    r     |        x    |       q    -"<<"\n";
-	cout<< "-  sq root |       clear |     quit   -"<<"\n";
+	cout<< "-    /     |        x    |       q    -"<<"\n";
+	cout<< "-  Divide  |       clear |     quit   -"<<"\n";
 	cout<< "---------------------------------------"<<"\n";
 }
 int main(){
+
 	double lval=0.0, rval=0.0;
-	double answer=0.0;
 	char oper_inp  = '\0';
 	show_calculator();
 	while (oper_inp != 'q')
 	{
+		cout<<"This program does basic calculation(+,-,*,/)	"
+			<<"Enter an operation, value1 and value 2 to calculate\n";
+
 		cout<<"Enter operation: ";
 		cin>>oper_inp;
 	if(oper_inp=='+'||oper_inp=='-'||oper_inp=='*'||oper_inp=='/')
@@ -63,13 +60,13 @@ int main(){
 	
 
 	switch(oper_inp){
-		case '+': answer = lval+rval; break;
-			  case '-': answer = lval-rval; break;
-				    case '*': answer = lval*rval; break;
-					      case '/': answer = lval/rval; break;
-					      default:cout<<"Answer: "<<answer <<'\n';break;}
+		case '+':  lval+=rval; break;
+			  case '-': lval-=rval; break;
+				    case '*': lval*=rval; break;
+					      case '/': lval/=rval; break;
+					      default:cout<<"Answer: "<<lval <<'\n';break;}
 						      if(oper_inp!='q'){
-						      display_output(answer); }
-						      else{cout<<"Bye.";};
+						      display_output(lval); }
+						      else{cout<<"Happy learning!!!.";};
 	}
 }
